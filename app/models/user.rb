747ec470db4e0ccf78ad4b0ddeebf401a78039b7
woshidan <bibro.pcg@gmail.com>
2015-05-19 23:37:51 +0900

    add before insert trigger test failed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  trigger.before(:insert) do
+    "same_name_user_count = COUNT(*) FROM users where users.name = NEW.name; IF same_name_user_count = 0 THEN INSERT INTO users (id, name) VALUES (NEW.id, NEW.name); END IF;"
+  end
 end
